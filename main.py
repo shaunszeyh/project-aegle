19cf1591a6c62ba504e4d5ec8101fe4a465ed6c1
shaun <shaunszeyh@gmail.com>
2022-06-15 15:13:26 +0800

    Update main.py and clean code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
-from sklearn import tree, metrics, neighbors
+from sklearn import tree, metrics, neighbors, svm
 from sklearn.model_selection import train_test_split
 
 # Cleaning up the data (dropping columns, setting label, etc)
@@ -19,18 +19,23 @@ df["smoking_status_n"] = le_ever_married.fit_transform(df["smoking_status"])
 target = df["stroke"]
 df_n = df[["age", "gender_n", "hypertension", "heart_disease", "ever_married_n", "work_type_n", "avg_glucose_level", "bmi", "smoking_status_n",]]
 
-# Trying with decision tree (Accuracy around 90%)
-decision_tree = tree.DecisionTreeClassifier()
+# Create train test split
 X_train, X_test, y_train, y_test = train_test_split(df_n, target, test_size=0.2)
-decision_tree.fit(X_train, y_train)
-decision_tree_prediction = decision_tree.predict(X_test)
-decision_tree_accuracy = metrics.accuracy_score(y_test, decision_tree_prediction)
-print("Accuracy for decision tree:", decision_tree_accuracy)
 
-# Trying with K-Nearest Neighbours (Accuracy around)
-knn = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
-knn.fit(X_train, y_train)
-knn_prediction = knn.predict(X_test)
-knn_accuracy = metrics.accuracy_score(y_test, knn_prediction)
-print("Accuracy for KNN:", knn_accuracy)
+def get_accuracy(model): # run and return accuracy of a model
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    accuracy = metrics.accuracy_score(y_test, prediction)
+    return accuracy
 
+# Trying with decision tree (Accuracy around 90%)
+decision_tree_model = tree.DecisionTreeClassifier()
+print("Accuracy for Decision Tree:", get_accuracy(decision_tree_model))
+
+# Trying with K-Nearest Neighbours (Accuracy around 95%)
+knn_model = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
+print("Accuracy for KNN:", get_accuracy(knn_model))
+
+# Trying with Support Vector Machines (Accuracy exact match with KNN)
+svm_model = svm.SVC()
+print("Accuracy for SVM:", get_accuracy(svm_model))

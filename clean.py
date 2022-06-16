9e0cded398ed0f48d44bc9d7f1964eb778fcc09f
shaun <shaunszeyh@gmail.com>
2022-06-16 16:18:19 +0800

    Split up code into different parts

---
+++ clean.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+# Cleaning up the data (dropping columns, setting label, etc) and saving to a new csv
+df = pd.read_csv("healthcare-dataset-stroke-data.csv")
+df.fillna(df.mean(numeric_only=True), inplace=True)
+
+le_gender = LabelEncoder()
+le_ever_married = LabelEncoder()
+le_work_type = LabelEncoder()
+le_smoking_status = LabelEncoder()
+
+df["gender_n"] = le_gender.fit_transform(df["gender"])
+df["ever_married_n"] = le_ever_married.fit_transform(df["ever_married"])
+df["work_type_n"] = le_ever_married.fit_transform(df["work_type"])
+df["smoking_status_n"] = le_ever_married.fit_transform(df["smoking_status"])
+
+df.drop(["gender", "ever_married", "work_type", "smoking_status", "Residence_type"], axis=1, inplace=True)
+
+df.to_csv("healthcare-dataset-stroke-data-n.csv", sep=',')
--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
 from sklearn import tree, metrics, neighbors, svm
 from sklearn.model_selection import train_test_split
 
-# Cleaning up the data (dropping columns, setting label, etc)
-df = pd.read_csv("healthcare-dataset-stroke-data.csv")
-df.fillna(df.mean(numeric_only=True), inplace=True)
-le_gender = LabelEncoder()
-le_ever_married = LabelEncoder()
-le_work_type = LabelEncoder()
-le_smoking_status = LabelEncoder()
-df["gender_n"] = le_gender.fit_transform(df["gender"])
-df["ever_married_n"] = le_ever_married.fit_transform(df["ever_married"])
-df["work_type_n"] = le_ever_married.fit_transform(df["work_type"])
-df["smoking_status_n"] = le_ever_married.fit_transform(df["smoking_status"])
+# Get the columns needed
+df = pd.read_csv("healthcare-dataset-stroke-data-n.csv")
 target = df["stroke"]
 df_n = df[["age", "gender_n", "hypertension", "heart_disease", "ever_married_n", "work_type_n", "avg_glucose_level", "bmi", "smoking_status_n",]]
 
